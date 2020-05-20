c29cfc735938b73abce49b49520ab2911cdfc9fe
smnadalin <smnadalin@gmail.com>
2020-05-21 02:20:07 +1000

    Initial workings of frequency analysis methods

--- src/App.js
+++ src/App.js
@@ -6,23 +6,44 @@ class App extends React.Component
   constructor()
   {
     super();
-    this.state = {cipherText: ""};
+   
+    //Initialise state
+    this.state = {cipherText: "test",
+                  ...createAlphabetObject()};
   }
 
+  //Update changes to cipherText
   handleChange = (event) =>
   {
     this.setState(
       {cipherText: event.target.value}
     )
-
     event.preventDefault();
   }
 
+  //Submit cipherText for analysis
   handleSubmit = (event) =>
   {
     event.preventDefault();
+    this.letterFrequency(this.state.cipherText);
+
   }
 
+  //Counts the frequency of each letter
+  letterFrequency = (str) =>
+  {
+    for (let i = 0; i < str.length; i++)
+    {
+      let character = str.charAt(i);
+      console.log(character);
+      this.setState(prevState => (
+        {[character]: prevState[character] + 1}
+      )
+      )
+      
+    }
+    console.log(this.state);
+  }
 
   render() 
   {
@@ -35,9 +56,21 @@ class App extends React.Component
           <textarea onChange={this.handleChange} value={this.state.cipherText}></textarea>
           <button type="submit">Sumbit</button>
       </form>
+    <h4>A frequency is {this.state.a}, B frequency is {this.state.b}, C frequency is {this.state.c}...</h4>
       </div>
-    
   }
 }
 
 export default App;
+
+function createAlphabetObject()
+{
+  let firstLetter = "a".charCodeAt(0);
+  let lastLetter = "z".charCodeAt(0);
+  let alphabet = {};
+  for (let i = firstLetter; i <= lastLetter; i++)
+  {
+    alphabet[String.fromCharCode(i)] = 0;
+  }
+  return alphabet;
+}
